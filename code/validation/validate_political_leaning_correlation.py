c6e231a978ed42ece82d656a53b4a8d20fc7c0f7
yang3kc <yang3kc@gmail.com>
2025-01-11 20:00:49 -0500

    🔄 refactor(validation): update script to focus on political leaning scores

--- code/validation/validate_political_leaning_correlation.py
+++ code/validation/validate_political_leaning_correlation.py
@@ -1,16 +1,16 @@
 """
-This script validates the correlation between existing political bias labels and calculated political bias scores.
+This script validates the correlation between existing political bias labels and calculated political leaning scores.
 
-It reads the existing political bias labels and calculated political bias scores, merges them, and computes the Spearman correlation
-between each existing label and each calculated bias score metric. The results are saved to a CSV file.
+It reads the existing political bias labels and calculated political leaning scores, merges them, and computes the Spearman correlation
+between each existing label and each calculated leaning score metric. The results are saved to a CSV file.
 
 Usage:
-    python validate_political_bias_correlation.py <existing_political_bias_labels_path> <political_bias_score_path> <political_bias_score_partyreg_path> <output_path>
+    python validate_political_leaning_correlation.py <existing_political_bias_labels_path> <political_leaning_score_path> <political_leaning_score_partyreg_path> <output_path>
 
 Arguments:
     existing_political_bias_labels_path: Path to the CSV file containing existing political bias labels.
-    political_bias_score_path: Path to the parquet file containing calculated political bias scores.
-    political_bias_score_partyreg_path: Path to the parquet file containing calculated political bias scores for party registration.
+    political_leaning_score_path: Path to the parquet file containing calculated political leaning scores.
+    political_leaning_score_partyreg_path: Path to the parquet file containing calculated political leaning scores for party registration.
     output_path: Path to save the output CSV file containing the correlation results.
 """
 
@@ -19,25 +19,27 @@ import sys
 import scipy.stats
 
 existing_political_bias_labels_path = sys.argv[1]
-political_bias_score_path = sys.argv[2]
-political_bias_score_partyreg_path = sys.argv[3]
+political_leaning_score_path = sys.argv[2]
+political_leaning_score_partyreg_path = sys.argv[3]
 
 output_path = sys.argv[-1]
 
 existing_political_bias_labels_df = pd.read_csv(existing_political_bias_labels_path)
 
-political_bias_score_df = pd.read_parquet(political_bias_score_path)
-political_bias_score_partyreg_df = pd.read_parquet(political_bias_score_partyreg_path)
-political_bias_score_partyreg_df.rename(
+political_leaning_score_df = pd.read_parquet(political_leaning_score_path)
+political_leaning_score_partyreg_df = pd.read_parquet(
+    political_leaning_score_partyreg_path
+)
+political_leaning_score_partyreg_df.rename(
     columns={
-        "bias_score": "bias_score_reg",
+        "leaning_score": "leaning_score_reg",
     },
     inplace=True,
 )
 
-partisan_bias_score_merged = existing_political_bias_labels_df.merge(
-    political_bias_score_df, on="domain"
-).merge(political_bias_score_partyreg_df, on="domain")
+partisan_leaning_score_merged = existing_political_bias_labels_df.merge(
+    political_leaning_score_df, on="domain"
+).merge(political_leaning_score_partyreg_df, on="domain")
 
 classification_cols = [
     "fb_score",
@@ -51,10 +53,10 @@ classification_cols = [
 corr_list = []
 for col in classification_cols:
     for metric in [
-        "bias_score",
-        "bias_score_reg",
+        "leaning_score",
+        "leaning_score_reg",
     ]:
-        temp_df = partisan_bias_score_merged[[metric, col]].dropna()
+        temp_df = partisan_leaning_score_merged[[metric, col]].dropna()
         rho, p = scipy.stats.spearmanr(temp_df[metric], temp_df[col])
         corr_list.append([col, metric, rho, p, len(temp_df)])
 corr_df = pd.DataFrame(
