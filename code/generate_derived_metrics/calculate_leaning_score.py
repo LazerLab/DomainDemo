610a0894c02c3d78e9a7a23259c628b0fd88911d
yang3kc <yang3kc@gmail.com>
2025-01-11 19:49:14 -0500

    🔄 refactor(metrics): rename bias score script to leaning score and update arguments

--- code/generate_derived_metrics/calculate_leaning_score.py
+++ code/generate_derived_metrics/calculate_leaning_score.py
@@ -1,13 +1,13 @@
 """
-This script calculates the bias score for different domains based on binary demographic data.
+This script calculates the leaning scores for different domains based on binary demographic data.
 
 Usage:
-    python calculate_bias_score.py <univariate_path> <univariate_all_domains_path> <demo> <left_category> <right_category> <output_path>
+    python calculate_leaning_score.py <univariate_distribution_path> <univariate_baseline_path> <demo> <left_category> <right_category> <output_path>
 
 Arguments:
-    univariate_path: str
+    univariate_distribution_path: str
         The path to the input Parquet file containing univariate data. This file contain the user distribution of each domain across different categories in the demographic dimensions.
-    univariate_all_domains_path: str
+    univariate_baseline_path: str
         The path to the input Parquet file containing univariate data for all domains. This file contain the user distribution of all domains across different categories in the demographic dimensions and serves as the baseline.
     demo: str
         The demographic category to be used for the calculation.
@@ -16,24 +16,24 @@ Arguments:
     right_category: str
         The right category of the binary demographic dimension.
     output_path: str
-        The path to the output Parquet file where the bias score results will be saved.
+        The path to the output Parquet file where the leaning score results will be saved.
 """
 
 import pandas as pd
 import sys
 
-univariate_path = sys.argv[1]
-univariate_all_domains_path = sys.argv[2]
+univariate_distribution_path = sys.argv[1]
+univariate_baseline_path = sys.argv[2]
 demo = sys.argv[3]
 left_category = sys.argv[4]
 right_category = sys.argv[5]
 output_path = sys.argv[-1]
 
-univariate_df = pd.read_parquet(univariate_path)
-univariate_all_domains_df = pd.read_parquet(univariate_all_domains_path)
-univariate_all_domains_df.rename(columns={"users": "demo_total_users"}, inplace=True)
+univariate_distribution_df = pd.read_parquet(univariate_distribution_path)
+univariate_baseline_df = pd.read_parquet(univariate_baseline_path)
+univariate_baseline_df.rename(columns={"users": "demo_total_users"}, inplace=True)
 
-raw_count_binary_df = univariate_df.merge(univariate_all_domains_df, on=demo)
+raw_count_binary_df = univariate_distribution_df.merge(univariate_baseline_df, on=demo)
 
 raw_count_binary_df[f"users_portion"] = (
     raw_count_binary_df["users"] / raw_count_binary_df["demo_total_users"]
@@ -43,10 +43,10 @@ portion_df = raw_count_binary_df.pivot_table(
     index="domain", columns=demo, values="users_portion", fill_value=0
 ).reset_index()
 
-portion_df[f"bias_score"] = (portion_df[right_category] - portion_df[left_category]) / (
-    portion_df[right_category] + portion_df[left_category]
-)
+portion_df[f"leaning_score"] = (
+    portion_df[right_category] - portion_df[left_category]
+) / (portion_df[right_category] + portion_df[left_category])
 
 portion_df.dropna(inplace=True)
 
-portion_df[["domain", "bias_score"]].to_parquet(output_path, index=False)
+portion_df[["domain", "leaning_score"]].to_parquet(output_path, index=False)
